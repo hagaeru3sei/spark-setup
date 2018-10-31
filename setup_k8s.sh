2a9953e64add900a6cbd0ad6772b5f793bbcd070
Nobuaki Mochizuki <hagaeru3sei@gmail.com>
2018-10-31 16:15:30 +0900

    Add apt option

--- setup_k8s.sh
+++ setup_k8s.sh
@@ -5,13 +5,13 @@ echo 'deb https://download.virtualbox.org/virtualbox/debian xenial contrib' | su
 curl -s https://www.virtualbox.org/download/oracle_vbox_2016.asc | sudo apt-key add -
 curl -s https://www.virtualbox.org/download/oracle_vbox.asc | sudo apt-key add -
 sudo apt update
-sudo apt install virtualbox-5.2
+sudo apt install -y virtualbox-5.2
 
 # docker
 echo 'deb https://download.docker.com/linux/ubuntu zesty stable' | sudo tee /etc/apt/source.list.d/docker.list
 curl -s https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt update
-sudo apt install docker-ce
+sudo apt install -y docker-ce
 sudo curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
