1afc05a40ca80f65bac8e967a67470ff398f9935
Nobuaki Mochizuki <hagaeru3sei@gmail.com>
2018-11-27 18:28:31 +0900

    Refactor

--- setup_dataproc.sh
+++ setup_dataproc.sh
@@ -12,6 +12,36 @@ pip3 install --upgrade wheel
 pip3 install --upgrade numpy
 
 # Setup python3 for Dataproc
-echo "export PYSPARK_PYTHON=python3" | tee -a /etc/profile.d/spark_config.sh /etc/bash.bashrc /usr/lib/spark/conf/spark-env.sh
-echo "export PYTHONHASHSEED=0" | tee -a /etc/profile.d/spark_config.sh /etc/bash.bashrc /usr/lib/spark/conf/spark-env.sh
-echo "spark.executorEnv.PYTHONHASHSEED=0" | tee -a /etc/spark/conf/spark-defaults.conf
+env1="export PYSPARK_PYTHON=python3"
+env2="export PYTHONHASHSEED=0"
+env3="spark.executorEnv.PYTHONHASHSEED=0"
+
+conf1="/etc/profile.d/spark_config.sh"
+conf2="/etc/bash.bashrc"
+conf3="/usr/lib/spark/conf/spark-env.sh"
+conf4="/etc/spark/conf/spark-defaults.conf"
+
+if [ $(grep -c $env1 $conf1) -lt 1 ]; then
+  echo $env1 | tee -a $conf1
+fi
+if [ $(grep -c $env1 $conf2) -lt 1 ]; then
+  echo $env1 | tee -a $conf2
+fi
+if [ $(grep -c $env1 $conf3) -lt 1 ]; then
+  echo $env1 | tee -a $conf3
+fi
+
+if [ $(grep -c $env2 $conf1) -lt 1 ]; then
+  echo $env2 | tee -a $conf1
+fi
+if [ $(grep -c $env2 $conf2) -lt 1 ]; then
+  echo $env2 | tee -a $conf2
+fi
+if [ $(grep -c $env2 $conf3) -lt 1 ]; then
+  echo $env2 | tee -a $conf3
+fi
+
+if [ $(grep -c $env3 $conf4) -lt 1 ]; then
+  echo $env3 | tee -a $conf4
+fi
+
